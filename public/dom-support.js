d810a5bd49674221abbde69515afd0d5e3f4d993
Agent421 <eakoledoye@gmail.com>
2023-08-15 21:02:26 +0100

    double quotes

--- public/dom-support.js
+++ public/dom-support.js
@@ -1,82 +1,82 @@
-import { find_points_needed, hl_num, ol_num } from './home';
+import { find_points_needed, hl_num, ol_num } from "./home";
 
 export function set_sys_theme() {
 
-  var r = document.querySelector(':root');
+  var r = document.querySelector(":root");
 
-  const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
   if (darkThemeMq.matches) {
-    r.style.setProperty('--white', 'black');
-    r.style.setProperty('--black', 'white');
+    r.style.setProperty("--white", "black");
+    r.style.setProperty("--black", "white");
 
-    const cyan_text = document.querySelectorAll('info_output');
+    const cyan_text = document.querySelectorAll("info_output");
 
     cyan_text.forEach(text => {
-      text.style.color = 'cyan';
+      text.style.color = "cyan";
     });
 
   }
   else {
-    r.style.setProperty('--white', 'white');
-    r.style.setProperty('--black', 'black');
+    r.style.setProperty("--white", "white");
+    r.style.setProperty("--black", "black");
   }
 }
 
-document.getElementById('bool_hl_maths').addEventListener('click', is_hl_maths);
+document.getElementById("bool_hl_maths").addEventListener("click", is_hl_maths);
 
 export var add_25 = 25;
 var is_hl_maths_counter = 1;
 var counter = 1;
 function is_hl_maths() {
-  var bool_hl_maths = document.getElementById('bool_hl_maths');
-  var adding_25_text = document.getElementById('adding_25_text');
+  var bool_hl_maths = document.getElementById("bool_hl_maths");
+  var adding_25_text = document.getElementById("adding_25_text");
   if (counter == 0) {
-    bool_hl_maths.innerHTML = 'Yes';
-    adding_25_text.style.opacity = '1';
-    adding_25_text.style.transition = '0.2s';
+    bool_hl_maths.innerHTML = "Yes";
+    adding_25_text.style.opacity = "1";
+    adding_25_text.style.transition = "0.2s";
     add_25 = 25;
     counter = 1;
 
   } else {
-    bool_hl_maths.innerHTML = 'No';
-    adding_25_text.style.opacity = '0';
+    bool_hl_maths.innerHTML = "No";
+    adding_25_text.style.opacity = "0";
     add_25 = 0;
     counter = 0;
   }
   find_points_needed();
   if (is_hl_maths_counter > 1) {
-    bool_hl_maths.classList.toggle('selected-button');
+    bool_hl_maths.classList.toggle("selected-button");
   }
   is_hl_maths_counter += 1;
 }
 is_hl_maths();
 
-document.getElementById('lcvp').addEventListener('click', is_lcvp);
+document.getElementById("lcvp").addEventListener("click", is_lcvp);
 var is_lcvp_counter = 1;
 var lcvp_counter = 1;
 var lcvp;
 function is_lcvp() {
-  var lcvp_button_text = document.getElementById('lcvp');
-  var lcvp_text = document.getElementById('lcvp_text');
+  var lcvp_button_text = document.getElementById("lcvp");
+  var lcvp_text = document.getElementById("lcvp_text");
   if (lcvp_counter == 0) {
-    lcvp_button_text.innerHTML = 'Yes';
-    lcvp_text.innerHTML = 'Doing LCVP modules';
-    lcvp_text.style.opacity = '1';
-    lcvp_text.style.transition = '0.2s';
+    lcvp_button_text.innerHTML = "Yes";
+    lcvp_text.innerHTML = "Doing LCVP modules";
+    lcvp_text.style.opacity = "1";
+    lcvp_text.style.transition = "0.2s";
     lcvp_counter = 1;
     lcvp = true;
 
-    lcvp_button_text.classList.toggle('red-borders');
+    lcvp_button_text.classList.toggle("red-borders");
   } else {
-    lcvp_button_text.innerHTML = 'No';
-    lcvp_text.style.opacity = '0';
+    lcvp_button_text.innerHTML = "No";
+    lcvp_text.style.opacity = "0";
     lcvp_counter = 0;
     lcvp = false;
   }
 
   find_points_needed();
   if (is_lcvp_counter > 1) {
-    lcvp_button_text.classList.toggle('selected-button');
+    lcvp_button_text.classList.toggle("selected-button");
   }
   is_lcvp_counter += 1;
   return lcvp;
@@ -84,22 +84,22 @@ function is_lcvp() {
 
 is_lcvp();
 
-document.getElementById('adding_25_container').style.display = 'none';
+document.getElementById("adding_25_container").style.display = "none";
 
 function adjustor(letter_grades) {
-  var CHANGEABLES = ['h5', 'h6', 'h7', 'o1', 'o2', 'o3'];
-  var HL_SUBS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7'];
+  var CHANGEABLES = ["h5", "h6", "h7", "o1", "o2", "o3"];
+  var HL_SUBS = ["h1", "h2", "h3", "h4", "h5", "h6", "h7"];
 
   var counted_hl = 0;
   var counted_ol = 0;
 
   var dict_changeables = {
-    'h5': 'o1',
-    'h6': 'o2',
-    'h7': 'o3',
-    'o1': 'h5',
-    'o2': 'h6',
-    'o3': 'h7',
+    "h5": "o1",
+    "h6": "o2",
+    "h7": "o3",
+    "o1": "h5",
+    "o2": "h6",
+    "o3": "h7",
   };
 
   var hl_index_changeables = [];
@@ -152,18 +152,18 @@ function adjustor(letter_grades) {
 
   if (lcvp) {
     var lcvp_modules = {
-      66: 'Distinction',
-      46: 'Merit',
-      28: 'Pass'
+      66: "Distinction",
+      46: "Merit",
+      28: "Pass"
     };
 
-    var lcvp_grades = ['h4', 'h6', 'o2', 'o4'];
+    var lcvp_grades = ["h4", "h6", "o2", "o4"];
 
     var lcvp_points = {
-      'h4': 66,
-      'h6': 46,
-      'o2': 46,
-      'o4': 28
+      "h4": 66,
+      "h6": 46,
+      "o2": 46,
+      "o4": 28
     };
 
     var collected_lcvp_points = [];
@@ -194,60 +194,60 @@ export function gar_and_ptg(points_needed) {
   points_needed = points_needed.sort();
 
   var mixed_ranks = {
-    100: 'h1',
-    88: 'h2',
-    77: 'h3',
-    66: 'h4',
+    100: "h1",
+    88: "h2",
+    77: "h3",
+    66: "h4",
 
-    56: 'h5/o1',
-    46: 'h6/o2',
-    37: 'h7/o3',
-    28: 'o4',
-    20: 'o5',
-    12: 'o6',
+    56: "h5/o1",
+    46: "h6/o2",
+    37: "h7/o3",
+    28: "o4",
+    20: "o5",
+    12: "o6",
   };
 
   if (hl_num > 0) {
-    mixed_ranks[56] = 'h5';
-    mixed_ranks[46] = 'h6';
-    mixed_ranks[37] = 'h7';
+    mixed_ranks[56] = "h5";
+    mixed_ranks[46] = "h6";
+    mixed_ranks[37] = "h7";
   }
 
   if (ol_num > 0) {
-    mixed_ranks[56] = 'o1';
-    mixed_ranks[46] = 'o2';
-    mixed_ranks[37] = 'o3';
+    mixed_ranks[56] = "o1";
+    mixed_ranks[46] = "o2";
+    mixed_ranks[37] = "o3";
   }
 
   var ranks = {
-    'h1': 90,
-    'h2': 80,
-    'h3': 70,
-    'h4': 60,
-    'h5': 56,
-    'h6': 46,
-    'h7': 37,
+    "h1": 90,
+    "h2": 80,
+    "h3": 70,
+    "h4": 60,
+    "h5": 56,
+    "h6": 46,
+    "h7": 37,
 
-    'Distinction': 66,
-    'Merit': 46,
-    'Pass': 28,
+    "Distinction": 66,
+    "Merit": 46,
+    "Pass": 28,
 
-    'o1': 56,
-    'o2': 46,
-    'o3': 37,
-    'o4': 28,
-    'o5': 20,
-    'o6': 12,
+    "o1": 56,
+    "o2": 46,
+    "o3": 37,
+    "o4": 28,
+    "o5": 20,
+    "o6": 12,
   };
 
   if (hl_num > 0) {
-    ranks['h5/o1'] = 50;
-    ranks['h6/o2'] = 40;
-    ranks['h7/o3'] = 30;
+    ranks["h5/o1"] = 50;
+    ranks["h6/o2"] = 40;
+    ranks["h7/o3"] = 30;
   } else {
-    ranks['h5/o1'] = 90;
-    ranks['h6/o2'] = 80;
-    ranks['h7/o3'] = 70;
+    ranks["h5/o1"] = 90;
+    ranks["h6/o2"] = 80;
+    ranks["h7/o3"] = 70;
   }
 
   var grades_soultion = [];
@@ -259,24 +259,24 @@ export function gar_and_ptg(points_needed) {
     }
   }
   total = total / (hl_num + ol_num);
-  var percentage_avg = String(Math.round(total)) + '%';
+  var percentage_avg = String(Math.round(total)) + "%";
   grades_soultion = adjustor(grades_soultion);
 
   return [percentage_avg, grades_soultion];
 }
 
 export function display_plus_25(bool_hl_maths) {
-  var adding_25_container = document.getElementById('adding_25_container');
+  var adding_25_container = document.getElementById("adding_25_container");
   if (bool_hl_maths) {
-    adding_25_container.style = 'display: block;color: #0066ff';
+    adding_25_container.style = "display: block;color: #0066ff";
   } else {
-    adding_25_container.style.display = 'none';
+    adding_25_container.style.display = "none";
   }
 }
 
 export function red_commas(grades) {
   grades = grades.toString();
-  grades = grades.replaceAll(',', '<strong class=\'important-red\'>,</strong>');
+  grades = grades.replaceAll(",", "<strong class='important-red'>,</strong>");
 
   return grades;
 }
@@ -356,18 +356,18 @@ export function main(target, hl_subs, ol_subs, maths_plus_25) {
 }
 
 function update_inputs() {
-  var target_input = document.getElementById('target_text');
-  target_input.addEventListener('input', function () {
+  var target_input = document.getElementById("target_text");
+  target_input.addEventListener("input", function () {
     find_points_needed();
   });
 
-  var hl_input = document.getElementById('hl_subs_text');
-  hl_input.addEventListener('input', function () {
+  var hl_input = document.getElementById("hl_subs_text");
+  hl_input.addEventListener("input", function () {
     find_points_needed();
   });
 
-  var ol_input = document.getElementById('ol_subs_text');
-  ol_input.addEventListener('input', function () {
+  var ol_input = document.getElementById("ol_subs_text");
+  ol_input.addEventListener("input", function () {
     find_points_needed();
   });
 }
@@ -377,10 +377,10 @@ update_inputs();
 function motivate() {
   seconds += 1;
   if (59 <= seconds && seconds <= 60) {
-    document.getElementById('img-info').src = 'images/joke.webp';
+    document.getElementById("img-info").src = "images/joke.webp";
   }
   else {
-    document.getElementById('img-info').src = 'images/points-system.webp';
+    document.getElementById("img-info").src = "images/points-system.webp";
   }
 
   if (seconds > 60) {
