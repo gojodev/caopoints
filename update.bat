efbe60136c35f8ceff8ce63fc72e881ff60f8adb
Agent421 <eakoledoye@gmail.com>
2023-10-23 09:12:17 +0100

    backups

--- update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-FOR /F "delims=" %%i in ( '""C:\Users\eakol\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\gcloud.cmd"" components copy-bundled-python'
-) DO (
-SET CLOUDSDK_PYTHON=%%i
-)
-
-npm run build && npm i -g firebase-tools && npm update -g && echo Y | gcloud components update
